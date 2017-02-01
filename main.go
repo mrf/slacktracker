dcb8ec1645fa0e6c7717a776b52e6adad5be1dd0
Mark Ferree <markferree@gmail.com>
2017-01-31 17:38:05 -0800

    Loading in config properly

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/nlopes/slack"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
@@ -14,7 +15,10 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(config))
+	token := strings.TrimPrefix(string(config), "token: ")
+	token = strings.TrimSpace(token)
 	api := slack.New(token)
+	spew.Dump(api)
 	users, err := api.GetUsers()
 	if err != nil {
 		fmt.Println("errr: ", err)
