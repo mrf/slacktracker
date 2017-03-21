82fcc4dd1e820df6b73a188701959bac1c48af07
Mark Ferree <markferree@gmail.com>
2017-03-21 15:13:04 -0700

    Better handling of time periods and rate limit slowdowns for API requests

--- main.go
+++ main.go
@@ -24,21 +24,25 @@ func main() {
 	token := strings.TrimPrefix(string(config), "token: ")
 	token = strings.TrimSpace(token)
 	api := slack.New(token)
+	//api.SetDebug(true)
 	users, err := api.GetUsers()
 	if err != nil {
-		fmt.Println("errr: ", err)
+		fmt.Println("Slack api error: ", err)
 		return
 	}
 	db, err := bolt.Open("slacktracker.db", 0600, nil)
 	if err != nil {
-		fmt.Println("Db not werking")
+		fmt.Println("Bolt DB not working, maybe forgot to run go get? Err: ", err)
 	}
 	defer db.Close()
 
-	// TODO actual meat
+	// TODO actual meat move?
 	fmt.Printf("date, ")
 	for _, user := range users {
 		if user.Presence != "" {
+			// Create a table for each user we find
+			// Do we need to react to users we haven't seen before differently?
+			// Probably only want to run this sporadically
 			db.Update(func(tx *bolt.Tx) error {
 				userBucket, err := tx.CreateBucket([]byte(user.Name))
 				if err != nil {
@@ -53,32 +57,30 @@ func main() {
 	}
 
 	// Idea being to open 1 channel per user (is this sustainable for large numbers?)
+	// DOes it work with bolt more importantly?
 
-	// TODO fix this to not run forever
-	t := time.Now()
-	for t.Month() < 100 {
-		t := time.Now()
-		if time.After(25*time.Second) != nil {
-			// run this every x seconds
-			fmt.Println()
-			// run this and keep it running
-			fmt.Printf(t.Format(time.RFC3339))
-			fmt.Printf(",")
-			users, err := api.GetUsers()
-			if err != nil {
-				fmt.Println("errr: ", err)
-				return
-			}
-			for _, user := range users {
-				// Ignore bots
-
-				if !user.IsBot {
-					// Check if presence empty (slackbot is special case)
-					if user.Presence != "" {
-						fmt.Printf(user.Presence)
-						// Todo don't print if this is last user
-						fmt.Printf(", ")
-					}
+	starttime := time.Now()
+	var runlength time.Duration = 5000 * time.Second
+	// Note, any value less than 15 seconds for frequency may get time out from slack API
+	var frequency time.Duration = 15 * time.Second
+	for time.Since(starttime) < runlength {
+		time.Sleep(frequency)
+		fmt.Println("")
+		fmt.Printf(time.Now().Format(time.RFC3339))
+		fmt.Printf(",")
+		users, err := api.GetUsers()
+		if err != nil {
+			spew.Dump(users)
+			spew.Dump(err)
+		}
+		for _, user := range users {
+			// Ignore bots
+			if !user.IsBot {
+				// Check if presence empty (slackbot is special case)
+				if user.Presence != "" {
+					fmt.Printf(user.Presence)
+					// Todo don't print if this is last user
+					fmt.Printf(", ")
 				}
 			}
 		}
